525532e0de383b4fbcee8e92cf3010074d37beb4
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2026-01-16 07:39:35 +0100

    init

---
+++ src/component/cmp_0500_locale/__init__.py
@@ -0,0 +1,36 @@
+"""Component locale - Init"""
+
+from app.config import Config  # pylint: disable=import-error
+
+
+def init_component(component, component_schema, _schema):
+    """Component - Init"""
+
+    languages = component_schema['data']['current']['site']['languages']
+
+    if len(languages) < 2 or not component['manifest']['config']['menu-navbar-enable']:
+        component_schema['inherit']['data']['navbar']['menu']['session:']['language'] = None
+        component_schema['inherit']['data']['navbar']['menu']['session:true']['language'] = None
+        return
+
+    # Language navbar menu for dropdown
+    menu = {
+        'name': 'Language',
+        'link': '',
+        'icon': 'x-icon-locale',
+        'prop': {},
+        'dropdown': {}
+    }
+
+    # create menu dropdown items
+    for lang in languages:
+        menu['dropdown'][lang] = {
+            'name': f'ref:locale:{lang}',
+            'link': f'?{Config.LANG_KEY}={lang}',
+            'icon': 'x-icon-bullet',
+            'prop': {}
+        }
+
+    # set menu in local data
+    component_schema['inherit']['data']['navbar']['menu']['session:']['language'] = menu
+    component_schema['inherit']['data']['navbar']['menu']['session:true']['language'] = menu
