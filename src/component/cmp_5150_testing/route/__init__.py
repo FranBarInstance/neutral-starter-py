1534cf9b1822a7d5c86ca2b4e06e0cbba8cfbfd6
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2026-03-05 09:50:48 +0100

    refactor: remove temporary testing component after RequestHandler migration

--- src/component/cmp_5150_testing/route/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Testing Blueprint Module."""
-
-from app.components import create_blueprint
-
-
-def init_blueprint(component, component_schema, _schema):
-    """Blueprint Init"""
-    bp = create_blueprint(component, component_schema)  # pylint: disable=unused-variable
-    from . import routes  # pylint: disable=import-error,C0415,W0611
