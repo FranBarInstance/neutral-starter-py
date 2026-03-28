c80031f6f27863b4edc384f08919e602301c44b5
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2026-03-28 18:05:18 +0100

    feat: add color theme support to menu components

--- src/component/cmp_0600_settheme/__init__.py
+++ src/component/cmp_0600_settheme/__init__.py
@@ -12,62 +12,86 @@ def init_component(component, component_schema, _schema):
 def menu_drawer(component, component_schema):
     """Component - Menu Drawer"""
     themes = component_schema['data']['current']['theme']['allow_themes']
+    colors = component_schema['data']['current']['theme']['allow_colors']
 
-    if len(themes) < 2 or not component['manifest']['config']['menu-drawer-enable']:
+    if len(themes) < 2 or not component['manifest']['config']['menu-theme-drawer-enable']:
         component_schema['data']['current']['menu']['session:']['setting']['theme'] = None
         component_schema['data']['current']['menu']['session:true']['setting']['theme'] = None
+    else:
+        menu = build_drawer_menu('Theme', 'x-icon-theme', themes, Config.THEME_KEY)
+        component_schema['data']['current']['menu']['session:']['setting']['theme'] = menu
+        component_schema['data']['current']['menu']['session:true']['setting']['theme'] = menu
+
+    if len(colors) < 2 or not component['manifest']['config']['menu-color-drawer-enable']:
+        component_schema['data']['current']['menu']['session:']['setting']['color'] = None
+        component_schema['data']['current']['menu']['session:true']['setting']['color'] = None
         return
 
-    # Language drawer menu for dropdown
-    menu = {
-        'name': 'Theme',
-        'link': '', # is a button, not a link
-        'icon': 'x-icon-theme',
-        'prop': {},
-        'dropdown': {}
-    }
-
-    # create menu dropdown items
-    for theme in themes:
-        menu['dropdown'][theme] = {
-            'text': theme,
-            'link': f'?{Config.THEME_KEY}={theme}',
-            'icon': 'x-icon-bullet',
-            'prop': {}
-        }
-
-    # set menu in local data
-    component_schema['data']['current']['menu']['session:']['setting']['theme'] = menu
-    component_schema['data']['current']['menu']['session:true']['setting']['theme'] = menu
+    menu = build_drawer_menu('Color', 'x-icon-color', colors, Config.THEME_COLOR_KEY)
+    component_schema['data']['current']['menu']['session:']['setting']['color'] = menu
+    component_schema['data']['current']['menu']['session:true']['setting']['color'] = menu
 
 
 def menu_navbar(component, component_schema):
     """Component - Menu Navbar"""
     themes = component_schema['data']['current']['theme']['allow_themes']
+    colors = component_schema['data']['current']['theme']['allow_colors']
 
-    if len(themes) < 2 or not component['manifest']['config']['menu-navbar-enable']:
+    if len(themes) < 2 or not component['manifest']['config']['menu-theme-navbar-enable']:
         component_schema['data']['navbar']['menu']['session:']['theme'] = None
         component_schema['data']['navbar']['menu']['session:true']['theme'] = None
+    else:
+        menu = build_navbar_menu('Theme', 'x-icon-theme', themes, Config.THEME_KEY)
+        component_schema['data']['navbar']['menu']['session:']['theme'] = menu
+        component_schema['data']['navbar']['menu']['session:true']['theme'] = menu
+
+    if len(colors) < 2 or not component['manifest']['config']['menu-color-navbar-enable']:
+        component_schema['data']['navbar']['menu']['session:']['color'] = None
+        component_schema['data']['navbar']['menu']['session:true']['color'] = None
         return
 
-    # Language navbar menu for dropdown
+    menu = build_navbar_menu('Color', 'x-icon-color', colors, Config.THEME_COLOR_KEY)
+    component_schema['data']['navbar']['menu']['session:']['color'] = menu
+    component_schema['data']['navbar']['menu']['session:true']['color'] = menu
+
+
+def build_drawer_menu(name, icon, values, config_key):
+    """Build drawer dropdown menu."""
     menu = {
-        'name': 'Theme',
-        'link': '',
-        'icon': 'x-icon-theme',
+        'name': name,
+        'link': '',  # is a button, not a link
+        'icon': icon,
         'prop': {},
         'dropdown': {}
     }
 
-    # create menu dropdown items
-    for theme in themes:
-        menu['dropdown'][theme] = {
-            'name': theme,
-            'link': f'?{Config.THEME_KEY}={theme}',
+    for value in values:
+        menu['dropdown'][value] = {
+            'text': value,
+            'link': f'?{config_key}={value}',
+            'icon': 'x-icon-bullet',
+            'prop': {}
+        }
+
+    return menu
+
+
+def build_navbar_menu(name, icon, values, config_key):
+    """Build navbar dropdown menu."""
+    menu = {
+        'name': name,
+        'link': '',
+        'icon': icon,
+        'prop': {},
+        'dropdown': {}
+    }
+
+    for value in values:
+        menu['dropdown'][value] = {
+            'name': value,
+            'link': f'?{config_key}={value}',
             'icon': 'x-icon-none',
             'prop': {}
         }
 
-    # set menu in local data
-    component_schema['data']['navbar']['menu']['session:']['theme'] = menu
-    component_schema['data']['navbar']['menu']['session:true']['theme'] = menu
+    return menu
